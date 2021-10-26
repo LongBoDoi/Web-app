a28ced1b31a63b8578e1658da771ac329d2ce464
NCT-NCT <62579946+NCT-NCT@users.noreply.github.com>
2021-10-26 07:35:20 +0700

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,23 @@
+function subsignin(){
+	
+}
+
+function subsignup(){
+}
+
+function choosesignin(){
+	document.getElementsByClassName("signin")[0].style.backgroundColor = "#ff3366";
+}
+
+function choosesignup(){
+	document.getElementsByClassName("signup")[0].style.backgroundColor = "#ff3366";
+}
+
+function leavesignin(){
+	document.getElementsByClassName("signin")[0].style.backgroundColor = "#000";
+}
+
+
+function leavesignup(){
+	document.getElementsByClassName("signup")[0].style.backgroundColor = "#000";
+}
