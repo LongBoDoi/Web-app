3d45cdb44b6c6ff600a6f64591d90250cc927018
NCT-NCT <62579946+NCT-NCT@users.noreply.github.com>
2021-10-26 16:40:24 +0700

    Add files via upload

--- Home/script.js
+++ Home/script.js
@@ -1,16 +1,13 @@
-function subsignin(){
-	
-}
 
-function subsignup(){
-}
 
 function choosesignin(){
 	document.getElementsByClassName("signin")[0].style.backgroundColor = "#ff3366";
+	document.getElementsByClassName("signin")[0].style.cursor = "pointer";
 }
 
 function choosesignup(){
 	document.getElementsByClassName("signup")[0].style.backgroundColor = "#ff3366";
+	document.getElementsByClassName("signup")[0].style.cursor = "pointer";
 }
 
 function leavesignin(){
@@ -20,4 +17,12 @@ function leavesignin(){
 
 function leavesignup(){
 	document.getElementsByClassName("signup")[0].style.backgroundColor = "#000";
+}
+
+function gosignin(){
+	window.location.assign("../signin/SignIn.html");
+}
+
+function gosignup(){
+	window.location.assign("../signup/SignUp.html");
 }
