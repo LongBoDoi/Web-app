36bb40f58944b9a41b1af54846e9faeba91fa808
LongBoDoiQua <manhlongnshd2001@gmail.com>
2021-10-31 15:44:37 +0700

    thanh ngao

--- signin/script.js
+++ signin/script.js
@@ -3,19 +3,19 @@ function sub(){
 	var valuePass = document.getElementById("password").value;
 	
 	if(valueUser != "Tên tài khoản"){
-		if(valueUser.length >= 8){
+		if(valueUser.length >= 8 && valueUser.length <= 20){
 			if(valuePass != "Mật khẩu"){
 				if(valuePass.length  >=8){
 					window.location.assign("../Main/Main.html");
 				}
-				else alert('Tài khoản hoặc Mật khẩu không hợp lệ1');	
+				else alert('Tài khoản hoặc Mật khẩu không hợp lệ');
 			}
-			else alert('Tài khoản hoặc Mật khẩu không hợp lệ2');
+			else alert('Tài khoản hoặc Mật khẩu không hợp lệ');
 		}
-		else alert('Tài khoản hoặc Mật khẩu không hợp lệ3');
+		else alert('Tài khoản hoặc Mật khẩu không hợp lệ');
 		
 	}
-	else alert('Tài khoản hoặc Mật khẩu không hợp lệ4');
+	else alert('Tài khoản hoặc Mật khẩu không hợp lệ');
 			
     	
 }
@@ -31,4 +31,15 @@ function forgotPass(){
 
 function rememberAcc(){
 
+}
+
+function thanh_focus() {
+	let usn = document.getElementById("username").value;
+	if (usn === "Tên tài khoản") {
+		document.getElementById("username").value = ''
+	}
+	let pwd = document.getElementById("password").value;
+	if (pwd === "Mật khẩu") {
+		document.getElementById("password").value = '';
+	}
 }
