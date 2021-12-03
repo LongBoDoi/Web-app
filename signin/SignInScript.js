1a5ee63ab624203debaa0d0aee40f6a50ecb2c28
NCT-NCT <62579946+NCT-NCT@users.noreply.github.com>
2021-12-03 10:53:34 +0700

    Add files via upload

--- signin/SignInScript.js
+++ signin/SignInScript.js
@@ -21,6 +21,16 @@ function choose_sign_in(){
     document.getElementById('sign_in').style.cursor = "pointer";
 }
 
+function choose_sign_up(){
+    document.getElementById('sign_up').style.cursor = "pointer";
+}
+
+
+
+function go_sign_up(){
+    window.location.assign('../signup/SignUp.html');
+}
+
 window.onload = function () {
     let log_in = window.sessionStorage.getItem("username");
     if (log_in !== null) {
